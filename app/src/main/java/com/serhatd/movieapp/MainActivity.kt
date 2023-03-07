0ade506f497094e3e6d3fe7a28d2725fd936fc21
serhat-demir <serhatdmr@protonmail.com>
2023-03-07 23:12:25 +0300

    setup theme & packages

--- app/src/main/java/com/serhatd/movieapp/MainActivity.kt
+++ app/src/main/java/com/serhatd/movieapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.serhatd.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.serhatd.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
