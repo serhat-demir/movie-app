a0944ee5b908f1b4512184f9e017a67544bc0ab0
serhat-demir <serhatdmr@protonmail.com>
2023-03-08 20:29:24 +0300

    update user repo

--- app/src/main/java/com/serhatd/movieapp/data/repository/UserRepository.kt
+++ app/src/main/java/com/serhatd/movieapp/data/repository/UserRepository.kt
@@ -1,42 +1,29 @@
 package com.serhatd.movieapp.data.repository
 
-import android.content.Context
+import com.serhatd.movieapp.data.entity.ApiResponseWithData
 import com.serhatd.movieapp.data.entity.User
 import com.serhatd.movieapp.data.entity.UserRequest
-import com.serhatd.movieapp.data.prefs.SharedPrefs
 import com.serhatd.movieapp.data.remote.UserService
-import com.serhatd.movieapp.ui.callback.ApiCallback
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
-class UserRepository(private val context: Context, private val callback: ApiCallback, private val service: UserService) {
-    suspend fun login(user_name: String, user_password: String) {
+class UserRepository(private val service: UserService) {
+    suspend fun login(user_name: String, user_password: String): Response<ApiResponseWithData<User>> {
         val userReq = UserRequest(user_name, user_password)
-        val response = service.login(userReq)
-
-        if (response.isSuccessful && response.body() != null) {
-            setCurrentSession(response.body()!!.data)
-            callback.onSuccess(response.code(), response.body()?.message ?: response.message())
-        } else {
-            callback.onError(response.code(), response.body()?.message ?: response.message())
+        return withContext(Dispatchers.IO) {
+            service.login(userReq)
         }
     }
 
-    suspend fun changePassword(new_password: String) {
-        val user = getCurrentSession()
-        user?.let {
-            user.user_password = new_password
-
-            val userReq = UserRequest(user.user_name, user.user_password)
-            val response = service.changePassword(user.user_id, userReq)
-
-            if (response.isSuccessful && response.body() != null) {
-                setCurrentSession(user)
-                callback.onSuccess(response.code(), response.body()!!.message)
-            } else {
-                callback.onError(response.code(), response.body()?.message ?: response.message())
-            }
+    suspend fun changePassword(user: User): Response<ApiResponseWithData<User>> {
+        val userReq = UserRequest(user.user_name, user.user_password)
+        return withContext(Dispatchers.IO) {
+            service.changePassword(user.user_id, userReq)
         }
     }
 
+    /*
     fun getCurrentSession(): User? {
         val user = User(
             SharedPrefs.getSharedPreference(context, SharedPrefs.COL_USER_ID, "0").toInt(),
@@ -59,4 +46,5 @@ class UserRepository(private val context: Context, private val callback: ApiCall
     fun removeCurrentSession() {
         SharedPrefs.removeSharedPreference(context, arrayOf(SharedPrefs.COL_USER_ID, SharedPrefs.COL_USER_NAME, SharedPrefs.COL_USER_PASSWORD))
     }
+    */
 }
