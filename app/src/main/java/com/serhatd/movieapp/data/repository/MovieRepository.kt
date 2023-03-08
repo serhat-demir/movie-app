deba0d8e042c3da1b1258c5aeb0d682639052411
serhat-demir <serhatdmr@protonmail.com>
2023-03-08 20:25:04 +0300

    update movie repo

--- app/src/main/java/com/serhatd/movieapp/data/repository/MovieRepository.kt
+++ app/src/main/java/com/serhatd/movieapp/data/repository/MovieRepository.kt
@@ -1,34 +1,38 @@
 package com.serhatd.movieapp.data.repository
 
 import android.content.Context
+import com.serhatd.movieapp.data.entity.ApiResponse
+import com.serhatd.movieapp.data.entity.ApiResponseWithData
+import com.serhatd.movieapp.data.entity.Movie
 import com.serhatd.movieapp.data.entity.MovieRequest
 import com.serhatd.movieapp.data.remote.MovieService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class MovieRepository(private val context: Context, private val service: MovieService) {
-    suspend fun getMovies() {
-        withContext(Dispatchers.IO) {
+    suspend fun getMovies(): Response<ApiResponseWithData<List<Movie>>> {
+        return withContext(Dispatchers.IO) {
             service.getMovies()
         }
     }
 
-    suspend fun addMovie(movie_name: String, movie_image: String, movie_url: String) {
+    suspend fun addMovie(movie_name: String, movie_image: String, movie_url: String): Response<ApiResponse> {
         val movieReq = MovieRequest(movie_name, movie_image, movie_url)
-        withContext(Dispatchers.IO) {
+        return withContext(Dispatchers.IO) {
             service.addMovie(movieReq)
         }
     }
 
-    suspend fun editMovie(movie_id: Int, movie_name: String, movie_image: String, movie_url: String) {
+    suspend fun editMovie(movie_id: Int, movie_name: String, movie_image: String, movie_url: String): Response<ApiResponse> {
         val movieReq = MovieRequest(movie_name, movie_image, movie_url)
-        withContext(Dispatchers.IO) {
+        return withContext(Dispatchers.IO) {
             service.editMovie(movie_id, movieReq)
         }
     }
 
-    suspend fun deleteMovie(movie_id: Int) {
-        withContext(Dispatchers.IO) {
+    suspend fun deleteMovie(movie_id: Int): Response<ApiResponse> {
+        return withContext(Dispatchers.IO) {
             service.deleteMovie(movie_id)
         }
     }
