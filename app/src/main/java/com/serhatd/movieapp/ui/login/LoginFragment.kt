4da24b55f5b98d367cee7d46a60fcf41a5de881e
serhat-demir <serhatdmr@protonmail.com>
2023-03-12 14:33:59 +0300

    login fragment design

---
+++ app/src/main/java/com/serhatd/movieapp/ui/login/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.serhatd.movieapp.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.serhatd.movieapp.MainActivity
+import com.serhatd.movieapp.R
+import com.serhatd.movieapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        (requireActivity() as MainActivity).binding.toolbar.title = getString(R.string.title_login)
+
+        return binding.root
+    }
+}
